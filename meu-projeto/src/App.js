218ae1e7bd8b23cbc66dfffa19d1e64763c6edbe
Mauro Ferreira Lima junior <jrjrjuquia@gmail.com>
2022-07-30 16:18:12 -0300

    atualização
    react

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Olá React</h1>
+     <p>Meu Primeiro App</p>
+    </div>
+  );
+}
+
+export default App;
